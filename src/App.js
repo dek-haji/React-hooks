798bb42c6c6c35ee9d86360a2a442b8ab45f5628
dek-haji <deeeqhaji@gmail.com>
2020-02-19 13:08:38 -0600

    created new states

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
 
+
+
 function App() {
+  const [todos, setTodos] = useState([
+    { text: "Learn about React" },
+    { text: "LMeet friend for lunch" },
+    { text: "Build really ccol todo app" },
+  ]);
   
+
 }
