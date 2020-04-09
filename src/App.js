9cdc73804e8043f20089896207b137c4d2d8503e
dek-haji <deeeqhaji@gmail.com>
2020-04-09 16:04:44 -0500

    added new component file

--- src/App.js
+++ src/App.js
@@ -5,9 +5,24 @@ import React, { useState } from 'react';
 function App() {
   const [todos, setTodos] = useState([
     { text: "Learn about React" },
-    { text: "LMeet friend for lunch" },
+    { text: "Meet friend for lunch" },
     { text: "Build really ccol todo app" },
   ]);
-  
+  //?
+  const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo 
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+      </div>
+    </div>
+  )
 
 }
+export default App
